da4b1e931afddd4e702010542e5b1798cb407fec
Duy Tung Ha <44284106+DuyTungHa@users.noreply.github.com>
2019-06-21 16:37:34 +1000

    build

---
+++ client/build/precache-manifest.240eb9b93ff0e9c964339511b49e69a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41df143d3df8147b8750fea345123e89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34757e0a6763eda743f2",
+    "url": "/static/css/main.a53867c7.chunk.css"
+  },
+  {
+    "revision": "ffaa6989db1a49909462",
+    "url": "/static/js/2.1c035100.chunk.js"
+  },
+  {
+    "revision": "34757e0a6763eda743f2",
+    "url": "/static/js/main.6fe977e3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dff0fa0a5db147915cdb4d9887893915",
+    "url": "/static/media/errorPage.dff0fa0a.jpg"
+  },
+  {
+    "revision": "8e36a7529b1b170ed657f746177a8cb0",
+    "url": "/static/media/logo.8e36a752.jpg"
+  }
+]);
