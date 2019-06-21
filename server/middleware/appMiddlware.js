579e8c630a4b39ec8e35d18cdda6f0992a044c3f
Duy Tung Ha <44284106+DuyTungHa@users.noreply.github.com>
2019-06-21 12:28:30 +1000

    fix

--- server/middleware/appMiddlware.js
+++ server/middleware/appMiddlware.js
@@ -6,12 +6,6 @@ const override = require('method-override');
 // setup global middleware here
 
 module.exports = (app) => {
-  
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
   app.use(morgan('dev'));
   app.use('/public', express.static('server/public'));
   app.use(bodyParser.urlencoded({ extended: true }));
